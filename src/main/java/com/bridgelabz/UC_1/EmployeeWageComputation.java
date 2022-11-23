8874453d8667688806bdc7c5ea5385772814df57
codeaadi <adityadubey362@gmail.com>
2022-11-23 20:37:56 +0530

    Added: method to find final wage

--- src/main/java/com/bridgelabz/UC_1/EmployeeWageComputation.java
+++ src/main/java/com/bridgelabz/UC_1/EmployeeWageComputation.java
@@ -87,10 +87,50 @@ package com.bridgelabz.UC_1;
 			}
 			System.out.println("Total Employee Wage " + monthlyWage);
 		}
-		public static void main(String[] args) {
-			EmployeeWageComputation Emp = new EmployeeWageComputation();
-			Emp.employeeMonthlyWage();
-	}
+//		public static void main(String[] args) {
+//			EmployeeWageComputation Emp = new EmployeeWageComputation();
+//			Emp.employeeMonthlyWage();
+	
+
+		//UC-6
+		void employeeMonthlyWageWhileloop() {
+
+					int employeeWagePerHour = 20;
+					int workingDaysPerMonth = 20;
+					int employeeHrs = 0;
+					int employeeWage = 0;
+					int totalEmployeeHours=0;
+					int maximumHoursInMonth=100;
+					int totalWorkingDays=0;
+					while(totalEmployeeHours<= maximumHoursInMonth &&
+							totalWorkingDays<workingDaysPerMonth) {
+						totalWorkingDays++; {
+						int employeeCheck=(int)Math.floor(Math.random() *10) %3;
+						 switch(employeeCheck) {
+						 case 1 :
+							 employeeHrs=16;
+							 break;
+						 case 2 :
+							 employeeHrs=8;
+							 break;
+							 default:
+								 employeeHrs=0;
+
+						 }
+						 employeeWage = employeeHrs * employeeWagePerHour;
+
+						totalEmployeeHours += employeeHrs;
+						 System.out.println("Day#: " + totalWorkingDays + " EmpHour: " +employeeHrs);
+					}
+					int totalEmployeeWage= totalEmployeeHours * employeeWagePerHour;
+					System.out.println("Total Employee Wage " +totalEmployeeWage);
+					}
 		}
+					public static void main(String[] args) {
+						EmployeeWageComputation Emp = new EmployeeWageComputation();
+						Emp.employeeMonthlyWageWhileloop();
+					}
+		}
+		
 	
 	
