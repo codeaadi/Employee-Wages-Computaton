1defa9e0be28e78d3adafbd9996b3b84e7b44a66
codeaadi <adityadubey362@gmail.com>
2022-11-23 20:29:18 +0530

    Added method to find monthly wage

--- src/main/java/com/bridgelabz/UC_1/EmployeeWageComputation.java
+++ src/main/java/com/bridgelabz/UC_1/EmployeeWageComputation.java
@@ -55,10 +55,42 @@ package com.bridgelabz.UC_1;
 		System.out.println("empWage : =" + empWage);
 		}
 
+//		public static void main(String[] args) {
+//			EmployeeWageComputation  Emp = new EmployeeWageComputation ();
+//				Emp.checkPartimeandFulltimeWages();
+	}
+	//UC-5----------------
+		void employeeMonthlyWage() {
+			int fullTime = 2;
+			int partTime = 1;
+			int employeeWagePerHour = 20;
+			int workingDaysPerMonth = 20;
+			int employeeHrs = 0;
+			int employeeWage = 0;
+			int monthlyWage = 0;
+			for (int day = 0; day < workingDaysPerMonth; day++) {
+				int employeeCheck = (int) Math.floor(Math.random() * 10) % 3;
+				switch (employeeCheck) {
+				case 1:
+					employeeHrs = 16;
+					break;
+				case 2:
+					employeeHrs = 8;
+					break;
+				default:
+					employeeHrs = 0;
+
+				}
+				employeeWage = employeeHrs * employeeWagePerHour;
+				monthlyWage += employeeWage;
+				System.out.println("Employee wage:" + employeeWage);
+			}
+			System.out.println("Total Employee Wage " + monthlyWage);
+		}
 		public static void main(String[] args) {
-			EmployeeWageComputation  Emp = new EmployeeWageComputation ();
-				Emp.checkPartimeandFulltimeWages();
+			EmployeeWageComputation Emp = new EmployeeWageComputation();
+			Emp.employeeMonthlyWage();
 	}
-	}
+		}
 	
 	
